0e9d77b6eab4ae185f4335245e0ced66c5988ea2
Guilherme Nepomuceno de Carvalho <guilhermenepomuceno46@gmail.com>
2026-06-22 21:14:57 -0300

    Update version to 1.0.8 and enhance build process with new tasks for copying README and assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, series, parallel } = require('gulp');
 
 task('build:icons', copyIcons);
+task('build:readme', copyReadme);
+task('build:assets', parallel(copyIcons, copyReadme));
 
 function copyIcons() {
   const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
   const nodeDestination = path.resolve('dist', 'nodes');
 
-  src(nodeSource).pipe(dest(nodeDestination));
-
   const credSource = path.resolve('credentials', '**', '*.{png,svg}');
   const credDestination = path.resolve('dist', 'credentials');
 
-  return src(credSource).pipe(dest(credDestination));
+  return Promise.all([
+    new Promise((resolve, reject) => {
+      src(nodeSource)
+          .pipe(dest(nodeDestination))
+          .on('end', resolve)
+          .on('error', reject);
+    }),
+    new Promise((resolve, reject) => {
+      src(credSource)
+          .pipe(dest(credDestination))
+          .on('end', resolve)
+          .on('error', reject);
+    }),
+  ]);
+}
+
+function copyReadme() {
+  return src('README.md').pipe(dest('dist'));
 }
